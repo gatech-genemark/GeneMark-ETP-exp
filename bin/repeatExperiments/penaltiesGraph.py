3579cbb702e5071c8654b5a7159fa5c9619ab1f5
Tomas Bruna <bruna.tomas@gmail.com>
2022-05-22 21:27:51 +0000

    Add option to show the estimated penalty value

--- bin/repeatExperiments/penaltiesGraph.py
+++ bin/repeatExperiments/penaltiesGraph.py
@@ -9,7 +9,8 @@
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
+import itertools
 
 def processData(data):
     data["F1"] = 2 * data["Sn"] * data["Sp"] / (data["Sn"] + data["Sp"])
@@ -17,39 +18,64 @@ def processData(data):
     data = data[:-1]
     return overallSnSp, data
 
+def flip(items, ncol):
+    return itertools.chain(*[items[i::ncol] for i in range(ncol)])
 
-def plot(data, output, title, ymin, ymax):
+def plot(data, output, title, ymin, ymax, accType, selected):
+
+    offset = 2
+    if not ymin:
+        ymin = (min(min(data["Sn"]), min(data["Sp"]))) - offset
+    if not ymax:
+        ymax = (max(max(data["Sn"]), max(data["Sp"]))) + offset
 
     overallSnSp, acc = processData(data)
 
-    #fig, ax1 = plt.subplots(figsize=(6.4, 5.8))
-    fig, ax1 = plt.subplots()
+    s = 1.5
+    fig, ax1 = plt.subplots(figsize=(6.4 / s, 4.8 / s))
 
     snColor = "tab:blue"
     spColor = "tab:orange"
     f1Color = "black"
 
+
+
+
+
     ax1.plot(acc["penalty"], acc["Sn"], linestyle='-', marker='.',
-             lw=1, color=snColor, label='Sn')
+             lw=1, color=snColor, label=accType + ' Sn')
+
     ax1.plot(acc["penalty"], acc["Sp"], linestyle='-', marker='.',
-             lw=1, color=spColor, label='Sp')
+             lw=1, color=spColor, label=accType + ' Sp')
+
     ax1.plot(acc["penalty"], acc["F1"], linestyle='-', marker='.',
-             lw=1, color=f1Color, label='F1')
+             lw=1, color=f1Color, label=accType + ' F1')
+    
+    ax1.plot(np.NaN, np.NaN, '-', color='none', label=' ')
+
+    ax1.axvline(x = selected * 100, color="black", lw=0.8, linestyle=':',
+                label = "Estimated penalty value")
+
+
 
     ax1.axhline(float(overallSnSp["Sn"]), 0, 1, color=snColor,
-                lw=0.8, linestyle='--',  label='Baseline Sn')
+                lw=0.8, linestyle='--')
     ax1.axhline(float(overallSnSp["Sp"]), 0, 1, color=spColor,
-                lw=0.8, linestyle='--', label='Baseline Sp')
+                lw=0.8, linestyle='--')
     ax1.axhline(float(overallSnSp["F1"]), 0, 1, color=f1Color,
-                lw=0.8, linestyle='--', label='Baseline F1')
+                lw=0.8, linestyle='--')
 
-    ax1.set_xlabel("Repeat penalty (negative)")
+    ax1.set_xlabel("Repeat penalty")
     ax1.set_ylabel("Accuracy (%)")
 
     ax1.set_ylim(ymin, ymax)
 
-    #ax1.legend(loc='upper center',  bbox_to_anchor=(0.5, -0.15), ncol=2)
-    ax1.legend(loc='lower left')
+    handles, labels = ax1.get_legend_handles_labels()
+    l2 = ax1.legend([handles[4]], [labels[4]], loc='lower left', frameon=False)
+    ax1.legend(flip(handles[0:4], 3), flip(labels[0:4], 3), loc='lower left',
+               ncol=3, handletextpad=0.5, columnspacing=1)
+    a = plt.gca().add_artist(l2)
+    a.set(zorder=10)
 
     plt.title(title.replace("_", " "))
     ax1.tick_params(axis="x", labelsize=7)
@@ -63,7 +89,8 @@ def plot(data, output, title, ymin, ymax):
 def main():
     args = parseCmd()
     data = pd.read_csv(args.input, sep="\t")
-    plot(data, args.output, args.title, args.ymin, args.ymax)
+    plot(data, args.output, args.title, args.ymin, args.ymax, args.type,
+         args.selected)
 
 
 def parseCmd():
@@ -73,8 +100,10 @@ def parseCmd():
     parser.add_argument('input', type=str)
     parser.add_argument('output', type=str)
     parser.add_argument('--title', default="", type=str)
-    parser.add_argument('--ymin', type=float, default=0)
-    parser.add_argument('--ymax', type=float, default=100)
+    parser.add_argument('--type', default="", type=str)
+    parser.add_argument('--ymin', type=float)
+    parser.add_argument('--ymax', type=float)
+    parser.add_argument('--selected', type=float)
 
     return parser.parse_args()
 
