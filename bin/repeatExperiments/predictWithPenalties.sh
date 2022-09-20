f3b94fa7bb69ef705088d1a198d6bf5020f0c6ed
Tomas Bruna <bruna.tomas@gmail.com>
2022-09-20 18:14:57 +0000

    Cleanup

--- bin/repeatExperiments/predictWithPenalties.sh
+++ bin/repeatExperiments/predictWithPenalties.sh
@@ -23,6 +23,7 @@ do
    cd "$i"
    ../../../../../bin/etp/gmes/gmes_petap.pl --soft_mask 1000 --mask_penalty "$i" --predict_with ../../../model/output.mod --seq ../../../nonhc/nonhc.fasta --cores 64 --verbose --evi ../../../nonhc/evi.gff --usr_cfg ../../../../../bin/etp/gmes/etp.cfg --max_gap 40000 --max_mask 40000 > loginfo
    ../../../../../bin/etp/format_back.pl genemark.gtf ../../../nonhc/nonhc.trace > nonhc.gtf
+   rm -r output data
    cd ..
 done
 
